4dcb54b202410a7510f1e34602e219033e94ee5f
IDPbIH <andrey.korneyev@gmail.com>
2021-12-09 12:02:21 +0300

    01-05

--- src/index.js
+++ src/index.js
@@ -3,20 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import productsList from './products.json';
 
-function Main() {
+function App() {
     return (
-        <div className="products-main">
-            <h1>Список товаров</h1>
-            <ul>
-                {productsList.map(
-                    function callback(p) {
-                        return p.id < 4 && <li key={p.id}>{p.name}</li>
-                    }
-                )}
-            </ul>
-        </div>
+        <main>
+            <div className="products-main">
+                <h1>Список товаров</h1>
+                <ul>
+                    {productsList.map(
+                        function callback(p) {
+                            return p.id < 4 && <li key={p.id}>{p.name}</li>
+                        }
+                    )}
+                </ul>
+            </div>
+        </main>
     );
 }
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<Main />, rootElement);
+ReactDOM.render(<App />, rootElement);
