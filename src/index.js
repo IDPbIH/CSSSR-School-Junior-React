cb1ff0ce723479137fce01886adfe5aa5fff56fe
Andrey <andrey.korneyev@gmail.com>
2021-12-10 21:48:45 +0300

    02-01

--- src/index.js
+++ src/index.js
@@ -3,18 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import productsList from './products.json';
 
+class ProductsMainH1 extends React.Component {
+    render() {
+        return <h1 className='products-main-h1'>Список товаров</h1>;
+    }
+}
+
+class ProductsMainUL extends React.Component {
+    render() {
+        return <ul className='products-main-ul'>
+            {productsList.map(
+                function callback(p) {
+                    return p.id < 4 && <li key={p.id}>{p.name}</li>
+                }
+            )}
+        </ul>;
+    }
+}
+
 function App() {
     return (
         <main>
-            <div className="products-main">
-                <h1>Список товаров</h1>
-                <ul>
-                    {productsList.map(
-                        function callback(p) {
-                            return p.id < 4 && <li key={p.id}>{p.name}</li>
-                        }
-                    )}
-                </ul>
+            <div className='products-main'>
+                <ProductsMainH1 />
+                <ProductsMainUL />
             </div>
         </main>
     );
