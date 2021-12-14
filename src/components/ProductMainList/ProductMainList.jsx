c7cf80aefb5b5741383d5b6d3cf435a5d501486b
IDPbIH <andrey.korneyev@gmail.com>
2021-12-14 17:11:09 +0300

    03-03

--- src/components/ProductMainList/ProductMainList.jsx
+++ src/components/ProductMainList/ProductMainList.jsx
@@ -2,31 +2,34 @@ import React from 'react';
 import styles from './ProductMainList.module.css';
 import ProductItem from 'csssr-school-product-card';
 import Star from '../Star/Star';
+import LogRender from '../../containers/LogRender/LogRender';
 
 const ratingComponent = ({ isFilled }) => {
     return (<div className={styles.star}>{isFilled ? <Star design='filled' /> : <Star design='empty' />}</div>);
 };
 
-const ProductMainList = ({shortProductList}) => { 
-    return (
-        <div className={styles.wrapper}>
-            {shortProductList.map(product => {
-                return (
-                    <ProductItem
-                        key={product.id}
-                        isInStock={product.isInStock}
-                        img={product.img}
-                        title={product.title}
-                        price={product.price}
-                        subPriceContent={' ' + product.subPriceContent}
-                        maxRating={product.maxRating}
-                        rating={product.rating}
-                        ratingComponent={ratingComponent}
-                    />
-                );
-            })}
-        </div >
-    );
+class ProductMainList extends LogRender {
+    render () {
+        return (
+            <div className={styles.wrapper}>
+                {this.props.shortProductList.map(product => {
+                    return (
+                        <ProductItem
+                            key={product.id}
+                            isInStock={product.isInStock}
+                            img={product.img}
+                            title={product.title}
+                            price={product.price}
+                            subPriceContent={' ' + product.subPriceContent}
+                            maxRating={product.maxRating}
+                            rating={product.rating}
+                            ratingComponent={ratingComponent}
+                        />
+                    );
+                })}
+            </div >
+        );
+    }
 }
 
 export default ProductMainList;
