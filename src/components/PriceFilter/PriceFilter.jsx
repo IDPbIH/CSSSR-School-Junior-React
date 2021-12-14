2d201a0d40c10ba0cb555932185221848acdeeda
IDPbIH <andrey.korneyev@gmail.com>
2021-12-14 16:30:48 +0300

    03-02

--- src/components/PriceFilter/PriceFilter.jsx
+++ src/components/PriceFilter/PriceFilter.jsx
@@ -1,54 +1,32 @@
 import React from 'react';
 import styles from './PriceFilter.module.css';
-import productList from '../../products.json';
 
-class PriceFilter extends React.Component {
-    constructor(props) {
-        super(props);
-
-        const productListPriceArray = productList.map(product => {
-            return (product.price);
-        });
-        this.state = {
-            minValue: Math.min.apply(null, productListPriceArray),
-            maxValue: Math.max.apply(null, productListPriceArray)
-        };
-    }
-
-    toggleActive = (e) => {
-        console.log(e);
-    }
-
-    handleSubmit = (e) => {
+const PriceFilter = ({ minValue, maxValue, changeState }) => {
+    let handleSubmit = (e) => {
         if (e.target[0].value >= 0 && e.target[1].value >= 0) {
             e.preventDefault();
-            this.setState({
-                minValue: e.target[0].value,
-                maxValue: e.target[1].value
-            });
+            changeState(e.target[0].value,e.target[1].value);
         } else {
             alert('Введите неотрицательное значение для фильтра!');
         }
     }
 
-    render() {
-        return (
-            <form className={styles.price_filter} onSubmit={this.handleSubmit}>
-                <div className={styles.price_filter_header}>Цена</div>
-                <div className={styles.price_filter_input}>
-                    от <input type="text"
-                        className={styles.price_filter_minValue}
-                        defaultValue={this.state.minValue} />
-                    до <input type="text"
-                        className={styles.price_filter_maxValue}
-                        defaultValue={this.state.maxValue} />
-                </div>
-                <div className={styles.price_filter_button}>
-                    <button>Применить</button>
-                </div>
-            </form>
-        );
-    }
+    return (
+        <form className={styles.price_filter} onSubmit={handleSubmit}>
+            <div className={styles.price_filter_header}>Цена</div>
+            <div className={styles.price_filter_input}>
+                от <input type="text"
+                    className={styles.price_filter_minValue}
+                    defaultValue={minValue} />
+                до <input type="text"
+                    className={styles.price_filter_maxValue}
+                    defaultValue={maxValue} />
+            </div>
+            <div className={styles.price_filter_button}>
+                <button>Применить</button>
+            </div>
+        </form>
+    );
 }
 
 export default PriceFilter;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import ProductsJSON from './products.json';
 import ProductMainHeader from './components/ProductMainHeader/ProductMainHeader';
-import ProductMainListContainer from './containers/ProductMainListContainer/ProductMainListContainer';
+import ProductMainList from './components/ProductMainList/ProductMainList';
 import PriceFilter from './components/PriceFilter/PriceFilter';
 
-function App() {
-    return (
-        <main>
-            <div className='products_main'>
-                <div className='box1'><ProductMainHeader /></div>
-                <div className='box2'><PriceFilter /></div>
-                <div className='box3'><ProductMainListContainer
-                                            minValue={150}
-                                            maxValue={200} />
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const productListPriceArray = ProductsJSON.map(product => {
+            return (product.price);
+        });
+
+        this.state = {
+            minPriceValue: Math.min.apply(null, productListPriceArray),
+            maxPriceValue: Math.max.apply(null, productListPriceArray),
+            products: ProductsJSON
+        };
+        
+        this.updateProducts(this.state.minPriceValue, this.state.maxPriceValue);
+    }
+
+    updateProducts = (minValue, maxValue) => {
+        this.shortProductList=this.state.products.filter(product => (product.price >= minValue && product.price <= maxValue)).slice(0,3);
+    }
+
+    changeState = (newMinValue, newMaxValue) => {
+        this.setState({
+            minPriceValue: newMinValue,
+            maxPriceValue: newMaxValue
+        });
+        this.updateProducts(newMinValue, newMaxValue);
+    }
+
+    render() {
+        return (
+            <main>
+                <div className='products_main'>
+                    <div className='box1'><ProductMainHeader /></div>
+                    <div className='box2'>
+                        <PriceFilter
+                            changeState={this.changeState}
+                            minValue={this.state.minPriceValue}
+                            maxValue={this.state.maxPriceValue}
+                        />
+                    </div>
+                    <div className='box3'>
+                        <ProductMainList shortProductList={this.shortProductList} />
+                    </div>
                 </div>
-            </div>
-        </main>
-    );
+            </main>
+        );
+    }
 }
 
 const rootElement = document.getElementById('root');
