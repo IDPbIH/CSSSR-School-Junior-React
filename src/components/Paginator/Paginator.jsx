3f3eea96a2cc061c6050b38741c50f33ebc9ffd0
IDPbIH <andrey.korneyev@gmail.com>
2022-01-19 17:32:17 +0300

    07-01a

---
+++ src/components/Paginator/Paginator.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Paginator extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Paginator;
