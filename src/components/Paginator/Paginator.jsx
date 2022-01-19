dfaa17642d7eeecd1d8d66b03a8abbc2c636ad1e
Andrey <andrey.korneyev@gmail.com>
2022-01-19 23:02:35 +0300

    07-01b

--- src/components/Paginator/Paginator.jsx
+++ src/components/Paginator/Paginator.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import styles from './Paginator.module.css';
 
 class Paginator extends React.Component {
     render() {
         return (
             <div>
-                
+                <button name='backButton' className={styles.back_button} onClick={this.handleClick}>Назад</button>
+                <span>1</span> 2 3 4 5
+                <button name='forwardButton' className={styles.forward_button} onClick={this.handleClick}>Вперёд</button>
             </div>
         );
     }
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,6 +3,7 @@ import styles from './ProductList.module.css';
 import Star from '../Star/Star';
 import LogRender from '../../components/LogRender/LogRender';
 import ProductItem from 'csssr-school-product-card';
+import Paginator from '../Paginator/Paginator';
 
 const ratingComponent = ({ isFilled }) => {
     return (<div className={styles.star}>{isFilled ? <Star design='filled' /> : <Star design='empty' />}</div>);
@@ -10,24 +11,29 @@ const ratingComponent = ({ isFilled }) => {
 
 class ProductList extends LogRender {
     render() {
+        const { filteredProducts } = this.props;
+
         return (
-            <div className={styles.wrapper}>
-                {this.props.filteredProducts.map(product => {
-                    return (
-                        <ProductItem
-                            key={product.id}
-                            isInStock={product.isInStock}
-                            img={product.img}
-                            title={product.title}
-                            price={product.price}
-                            subPriceContent={' ' + product.subPriceContent}
-                            maxRating={product.maxRating}
-                            rating={product.rating}
-                            ratingComponent={ratingComponent}
-                        />
-                    );
-                })}
-            </div >
+            <div>
+                <div className={styles.grid}>
+                    {filteredProducts.map(product => {
+                        return (
+                            <ProductItem
+                                key={product.id}
+                                isInStock={product.isInStock}
+                                img={product.img}
+                                title={product.title}
+                                price={product.price}
+                                subPriceContent={' ' + product.subPriceContent}
+                                maxRating={product.maxRating}
+                                rating={product.rating}
+                                ratingComponent={ratingComponent}
+                            />
+                        );
+                    })}
+                </div >
+                <Paginator/>
+            </div>
         );
     }
 }
